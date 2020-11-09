9928d7df5c825d55a1bd19c6924afe9de46c349c
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-11-09 11:09:13 -0600

    Basic OAuth practice

---
+++ spring-security-oauth-resource-server/src/main/java/com/kingshuk/security/spring/resourceserver/SpringSecurityOauthResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.kingshuk.security.spring.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthResourceServerApplication.class, args);
+	}
+
+}
---
+++ spring-security-oauth-resource-server/src/test/java/com/kingshuk/security/spring/resourceserver/SpringSecurityOauthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingshuk.security.spring.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
